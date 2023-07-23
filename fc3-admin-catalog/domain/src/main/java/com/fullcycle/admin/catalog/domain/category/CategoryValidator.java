5f4447c194ad2beb11dc1cd60bc2775c231464cb
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2023-07-23 14:01:27 -0300

    Implementando update de uma categoria

--- fc3-admin-catalog/domain/src/main/java/com/fullcycle/admin/catalog/domain/category/CategoryValidator.java
+++ fc3-admin-catalog/domain/src/main/java/com/fullcycle/admin/catalog/domain/category/CategoryValidator.java
@@ -10,6 +10,8 @@ import com.fullcycle.admin.catalog.domain.validation.Validator;
 public class CategoryValidator extends Validator {
 
     private final Category category;
+    private static final int NAME_MAX_LENGTH = 255;
+    private static final int NAME_MIN_LENGTH = 3;
 
     public CategoryValidator(final Category aCategory, final ValidationHandler aHandler) {
         super(aHandler);
@@ -34,7 +36,7 @@ public class CategoryValidator extends Validator {
         }
 
         final var length = name.trim().length();
-        if (length > 255 || length < 3) {
+        if (length > NAME_MAX_LENGTH || length < NAME_MIN_LENGTH) {
             this.validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
         }
     }
