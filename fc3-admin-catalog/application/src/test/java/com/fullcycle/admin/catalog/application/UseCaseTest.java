529bdb14ad40759c2316426cd74e90cec91d63b1
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2023-07-23 14:56:26 -0300

    Testes unitários do caso de uso de criação de categoria

--- fc3-admin-catalog/application/src/test/java/com/fullcycle/admin/catalog/application/UseCaseTest.java
+++ fc3-admin-catalog/application/src/test/java/com/fullcycle/admin/catalog/application/UseCaseTest.java
@@ -1,7 +1,10 @@
 package com.fullcycle.admin.catalog.application;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UseCaseTest {
 
     @Test
