6a12f6a6a5c61df22f0e730a93bb3567e3dd71d2
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2023-11-20 18:14:01 -0300

    Implementando o método update do gateway de categoria

--- fc3-admin-catalog/infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGateway.java
+++ fc3-admin-catalog/infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGateway.java
@@ -25,26 +25,30 @@ public class CategoryMySQLGateway implements CategoryGateway {
 
     @Override
     public Category create(final Category aCategory) {
-        return this.repository.save(CategoryJpaEntity.from(aCategory)).toAggregate();
+        return save(aCategory);
     }
 
     @Override
-    public void deleteById(CategoryID anId) {
+    public void deleteById(final CategoryID anId) {
 
     }
 
     @Override
-    public Optional<Category> findById(CategoryID anId) {
+    public Optional<Category> findById(final CategoryID anId) {
         return Optional.empty();
     }
 
     @Override
-    public Category update(Category aCategory) {
-        return null;
+    public Category update(final Category aCategory) {
+        return save(aCategory);
     }
 
     @Override
-    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+    public Pagination<Category> findAll(final CategorySearchQuery aQuery) {
         return null;
+    }
+
+    private Category save(final Category aCategory) {
+        return this.repository.save(CategoryJpaEntity.from(aCategory)).toAggregate();
     }
 }
