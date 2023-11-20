3836a7a8b347d49118e92f50ed1e48aec7de535f
Anderson O. Aristides <joyce.miranda@marisa.com.br>
2023-11-20 19:48:13 -0300

    Testes integrados do caso de uso de criação de categoria

--- fc3-admin-catalog/infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.java
+++ fc3-admin-catalog/infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.java
@@ -1,13 +1,8 @@
 package com.fullcycle.admin.catalog.infrastructure;
 
-import com.fullcycle.admin.catalog.application.category.create.CreateCategoryUseCase;
 import com.fullcycle.admin.catalog.infrastructure.configuration.WebServerConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
@@ -16,14 +11,6 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
-    }
-
-    @Bean
-    @DependsOnDatabaseInitialization
-    ApplicationRunner runner(@Autowired CreateCategoryUseCase createCategoryUseCase) {
-        return args -> {
-
-        };
     }
 
 }
